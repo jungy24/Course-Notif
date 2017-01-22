bf004d50ac36beab35e846387a192b490fb7243e
Jungyoon Yu <jungyoonyu@pal-nat186-76-205.itap.purdue.edu>
2017-01-21 20:41:24 -0500

    Adding project through first commit

--- Course Notifier/ViewController.swift
+++ Course Notifier/ViewController.swift
@@ -10,6 +10,8 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var classCountField: UITextField!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
@@ -20,6 +22,26 @@ class ViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
 
-
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        
+        //moving from city to another, and u send the furniture to the city ur moving to
+        let vc = segue.destination as! ViewController2
+        
+        vc.classCount = Int(classCountField.text!)
+        
+    }
+    
+    @IBAction func onNext(_ sender: Any) {
+        guard let value = Int(classCountField.text!) else {
+            print("value error")
+            return
+        }
+        
+        if value < 1 {
+            return
+        }
+        
+        performSegue(withIdentifier: "classSegue", sender: nil)
+    }
 }
 
---
+++ Course Notifier/ViewController2.swift
@@ -0,0 +1,64 @@
+//
+//  ViewController2.swift
+//  Course Notifier
+//
+//  Created by Jungyoon Yu on 1/21/17.
+//  Copyright © 2017 Jungyoon Yu. All rights reserved.
+//
+
+import UIKit
+
+class ViewController2: UIViewController {
+    
+    var classCount: Int?
+    
+    @IBOutlet weak var courseCountLabel: UILabel!
+    @IBOutlet weak var courseField: UITextField!
+    @IBOutlet weak var daysField: UITextField!
+    @IBOutlet weak var startField: UITextField!
+    @IBOutlet weak var endField: UITextField!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        print(classCount)
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+    @IBAction func getLast(_ sender: Any) {
+    }
+
+    @IBAction func onNext(_ sender: Any) {
+        let newCount = classCount! - 1
+    
+        if newCount == 0 {
+            print("classes have been added")
+            performSegue(withIdentifier: "classFinishSegue", sender: nil)
+        } else {
+            let next = self.storyboard?.instantiateViewController(withIdentifier: "class") as! ViewController2
+            
+            
+            
+            next.classCount = classCount! - 1
+            
+            self.navigationController?.pushViewController(next, animated: true)
+        }
+        
+        
+    }
+    
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    
+
+}
